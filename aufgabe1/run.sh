755c66ce57f563131692f71f3e4c830543949ccb
stormofice <58337328+stormofice@users.noreply.github.com>
2021-04-27 20:04:27 +0200

    another round

--- aufgabe1/run.sh
+++ aufgabe1/run.sh
@@ -3,7 +3,7 @@ mkdir lilo_test
 cd lilo_test
 cp ../lilo.c .
 echo "Retrieving test"
-curl "https://raw.githubusercontent.com/stormofice/sp-custom-tests/main/aufgabe1/test.c" -fsSL -o test.c
+curl -fsSL "https://raw.githubusercontent.com/stormofice/sp-custom-tests/main/aufgabe1/test.c" > test.c
 gcc -std=c11 -pedantic -DXOPENSOURCE=700 -Wall -Wl,--wrap=main -o lilo_test lilo.c test.c
 ./lilo_test
 exit="$?"
