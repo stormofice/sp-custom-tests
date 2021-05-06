edb7bb89293afb37257f066cdf2d6d90057ba5e6
stormofice <58337328+stormofice@users.noreply.github.com>
2021-05-06 21:05:40 +0200

    fixed controversial word list

--- aufgabe2/run.sh
+++ aufgabe2/run.sh
@@ -39,7 +39,6 @@ if [ "raw/null_byte_city" ]; then
     cd raw
     echo "Downloading new words lists"
     wget https://raw.githubusercontent.com/stormofice/sp-custom-tests/main/aufgabe2/raw/null_byte_city -q --show-progress
-    wget https://raw.githubusercontent.com/stormofice/sp-custom-tests/main/aufgabe2/raw/very_long_unicode -q --show-progress
     wget https://raw.githubusercontent.com/stormofice/sp-custom-tests/main/aufgabe2/raw/16k_unicode_sanitized-q --show-progress
     cd ..
 fi;
